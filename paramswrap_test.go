f5c20b84d2557f7623108eb30000c00444a42856
Mykola Humanov <morgenpurple@gmail.com>
2024-09-20 14:36:00 +0300

    paramswrap_test.go: add TestQueryParams

--- paramswrap_test.go
+++ paramswrap_test.go
@@ -1,6 +1,9 @@
 package urlp
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestParams_Encode(t *testing.T) {
 	tests := []struct {
@@ -32,6 +35,67 @@ func TestParams_Encode(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if gotEncode := tt.p.Encode(); gotEncode != tt.wantEncode {
 				t.Errorf("Params.Encode() = %v, want %v", gotEncode, tt.wantEncode)
+			}
+		})
+	}
+}
+
+func TestQueryParams(t *testing.T) {
+	type args struct {
+		rawQuery string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantP   Params
+		wantErr bool
+	}{
+		{
+			name:    "Empty",
+			args:    args{},
+			wantP:   Params{},
+			wantErr: false,
+		},
+		{
+			name:    "Ampersand as separator",
+			args:    args{"k1=v1&k2=v2"},
+			wantP:   Params{{"k1", "v1"}, {"k2", "v2"}},
+			wantErr: false,
+		},
+		{
+			name:    "Semicolon as separator",
+			args:    args{"k1=v1;k2=v2"},
+			wantP:   Params{{"k1", "v1"}, {"k2", "v2"}},
+			wantErr: false,
+		},
+		{
+			name:    "Mixed separators",
+			args:    args{"k1=v1;k2=v2&k3=v3"},
+			wantP:   Params{{"k1", "v1"}, {"k2", "v2"}, {"k3", "v3"}},
+			wantErr: false,
+		},
+		{
+			name:    "Encoded chars",
+			args:    args{`q=%22daily+news%22`},
+			wantP:   Params{{"q", `"daily news"`}},
+			wantErr: false,
+		},
+		{
+			name:    "Encoded chars err",
+			args:    args{`a=1&q=100%+truth&b=2&brightness=90%`},
+			wantP:   Params{{"a", "1"}, {"q", "100%+truth"}, {"b", "2"}, {"brightness", "90%"}},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotP, err := QueryParams(tt.args.rawQuery)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("QueryParams() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(gotP, tt.wantP) {
+				t.Errorf("QueryParams() = %v, want %v", gotP, tt.wantP)
 			}
 		})
 	}
